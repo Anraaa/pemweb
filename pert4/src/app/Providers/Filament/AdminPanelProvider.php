31682b4fc25d31e6b08104e0219aa984b8177b9d
Anraaa <aqlaharun03@gmail.com>
2025-04-21 18:47:20 +0700

    laravel blessed update

--- pert4/src/app/Providers/Filament/AdminPanelProvider.php
+++ pert4/src/app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('dashboard')
             ->spa()
             ->login()
             ->passwordReset()
@@ -49,10 +49,12 @@ class AdminPanelProvider extends PanelProvider
             ->discoverClusters(in: app_path('Filament/Admin/Clusters'), for: 'App\\Filament\\Admin\\Clusters')
             ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\\Filament\\Admin\\Widgets')
             ->widgets([
-                \Awcodes\Overlook\Widgets\OverlookWidget::class,
+                //\Awcodes\Overlook\Widgets\OverlookWidget::class,
             ])
             ->navigationGroups([
                 NavigationGroup::make()
+                    ->label('Product Manager'),
+                    NavigationGroup::make()
                     ->label('Administration'),
             ])
             ->userMenuItems([
